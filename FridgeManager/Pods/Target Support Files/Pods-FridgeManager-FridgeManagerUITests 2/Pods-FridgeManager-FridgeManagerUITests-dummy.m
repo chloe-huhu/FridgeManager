ebbeaa13de07acfaf4b70266661826108f99d9d5
ChloeHu <>
2020-12-30 10:25:07 +0800

    (update)

---
+++ FridgeManager/Pods/Target Support Files/Pods-FridgeManager-FridgeManagerUITests 2/Pods-FridgeManager-FridgeManagerUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FridgeManager_FridgeManagerUITests : NSObject
+@end
+@implementation PodsDummy_Pods_FridgeManager_FridgeManagerUITests
+@end
